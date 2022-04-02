987c9ee8776933f96125ab68bf4894b19fa35bab
marciniak_michal@outlook.com <marciniak_michal@outlook.com>
2022-04-02 12:07:23 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
 
@@ -8,3 +11,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def submit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.name_text_box.text
+
